c1a1bb54ab05eb19d2b1512313930fd44c68e857
cyh7834 <cyh7834@gmail.com>
2025-01-28 14:27:15 +0900

    Props로 데이터 전달하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,13 +2,24 @@ import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/footer'
+import Button from './components/Button'
 
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3
+  }
+
   return (
     <>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+      <Button {...buttonProps}></Button>
+      <Button text={"카페"}></Button>
+      <Button text={"블로그"}>
+        <Header></Header>
+      </Button>
     </>
   )
 }
