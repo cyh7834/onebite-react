accd3f92ac618d2060ad2d5c0b5c89222c96345e
cyh7834 <cyh7834@gmail.com>
2025-01-28 15:14:35 +0900

    State와 Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,28 +1,17 @@
 import './App.css'
-import { useState } from 'react'
+import Bulb from './components/Bulb'
+import Counter from './components/Counter'
 
+// 1. 자신이 관리하는 state가 변경 되었을 때
+// 2. 자신이 관리하는 props가 변경 되었을 때
+// 3. 부모 컴포넌트가 리 렌더링되면 자식도 리 렌더링 됨
+
+// 컴포넌트들의 state 들을 모두 부모에 넣는 것이 아니라 분리하는 것이 좋음
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
-
   return (
     <>
-    <div>
-      <h1>{light}</h1>
-      <button onClick={() => {
-        setLight(light === "ON" ? "OFF" : "ON")
-      }}>
-        {light === "ON" ? "끄기" : "켜기"}
-      </button>
-    </div>
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => {
-        setCount(count + 1)
-      }}>
-        +
-      </button>
-      </div>
+    <Bulb></Bulb>
+    <Counter></Counter>
     </>
   )
 }
