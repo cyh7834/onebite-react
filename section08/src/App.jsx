d89982681a54780aa26b2208dddaa190bf929950
cyh7834 <cyh7834@gmail.com>
2025-02-11 21:59:19 +0900

    투두리스트 Delete 투두 삭제하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
@@ -26,14 +26,43 @@ const mockData = [
 ];
 
 function App() {
-  const [todos, setTodos] = useState([mockData]);
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id : idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime()
+    }
+
+    setTodos([newTodo, ...todos]);
+  }
+
+  const onUpdate = (targetId) => {
+    setTodos(todos.map((todo) => {
+      if (todo.id === targetId) {
+        return {
+          ...todo,
+          isDone: !todo.isDone
+        }
+      }
+
+      return todo;
+    }))
+  }
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  }
 
   return (
     <>
     <div className='App'>
     <Header></Header>
-      <Editor></Editor>
-      <List></List>
+      <Editor onCreate={onCreate}></Editor>
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}></List>
     </div>
     </>
   )
