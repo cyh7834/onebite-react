89a1eb9d79eff4e131ec43e8243b90253803969f
cyh7834 <cyh7834@gmail.com>
2025-03-02 21:39:24 +0900

    Home 페이지 구현하기 2. 기능

--- section12/src/components/Button.jsx
+++ section12/src/components/Button.jsx
@@ -2,7 +2,7 @@ import "./Button.css";
 
 const Button = ({text, type, onClick}) => {
     return (
-    <button onClick={{onClick}} 
+    <button onClick={onClick} 
     className={`Button Button_${type}`}>{text}
     </button>);
 }
