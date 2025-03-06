56c9d4a0f60a873d5de3f74d1dc654e0d3f44d04
cyh7834 <cyh7834@gmail.com>
2025-03-06 20:56:57 +0900

    New 페이지 구현하기 2.기능

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -29,7 +29,7 @@ const mockData = [
 ]
 function reducer(state, action) {
   switch (action.type) {
-    case 'CREATE': return [action.Date, ...state];
+    case 'CREATE': return [action.data, ...state];
     case 'UPDATE': return state.map((item) => String(item.id) === String(action.data.id) ? action.data : item);
     case 'DELETE': return state.filter((item) => String(item.id) !== String(action.id));
   }
