29637d050d6c5aaf1eaf253f0cd5575769b7fe7e
cyh7834 <cyh7834@gmail.com>
2025-03-08 21:49:24 +0900

    Edit 페이지 구현하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -31,7 +31,7 @@ function reducer(state, action) {
   switch (action.type) {
     case 'CREATE': return [action.data, ...state];
     case 'UPDATE': return state.map((item) => String(item.id) === String(action.data.id) ? action.data : item);
-    case 'DELETE': return state.filter((item) => String(item.id) !== String(action.id));
+    case 'DELETE': return state.filter((item) => String(item.id) !== String(action.data.id));
   }
 }
 
