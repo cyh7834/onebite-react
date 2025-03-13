63fc3e4b0ad95dda31fde140abe6cce1b0826636
cyh7834 <cyh7834@gmail.com>
2025-03-13 21:29:43 +0900

    Diary 페이지 구현하기

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,9 +1,38 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+import useDiary from "../hooks/UseDiary";
 
 const Edit = () => {
     const params = useParams();
+    const nav = useNavigate();
+    const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
+    const curDiaryItem = useDiary(params.id);
+    
+    const onSubmit = (input) => {
+        if (window.confirm("일기를 정말 수정할까요?")) {
+            onUpdate(params.id, input.createdDate.getTime(), input.emotionId, input.content);
+            nav('/', {replace: true});
+        }
+    };
 
-    return <div>{params.id}번 일기 수정페이지입니다.</div>;
+    const onClickDelete = () => {
+        if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+            onDelete(params.id);
+            nav('/', {replace: true});
+        }
+    }
+
+    return <div>
+        <Header title={"일기 수정하기"}
+            leftChild={<Button onClick={()=> nav(-1)} text={"< 뒤로가기"}></Button>}
+            rightChild={<Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"}></Button>}>
+            </Header>
+            <Editor onSubmit={onSubmit} initData={curDiaryItem}></Editor>
+    </div>;
 }
 
 export default Edit;
